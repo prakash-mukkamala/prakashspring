da667ce91653accb46d34c1bc93eec307369467e
prakash <pmukkamala@gmail.com>
2021-01-26 01:34:32 -0500

    a

---
+++ src/main/java/com/prakashapp/PrakashspringApplication.java
@@ -0,0 +1,14 @@
+package com.prakashapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrakashspringApplication {
+
+	public static void main(String[] args) {
+		System.out.println ("PrakashSpringApp running");
+		SpringApplication.run(PrakashspringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/prakashapp/PrakashspringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prakashapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrakashspringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
